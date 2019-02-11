6a51d8b23f7e399e7e0b10731a7418286bf1a800
Harshmneet Arora <harshmeet_arora@yahoo.com>
2019-02-10 16:38:32 -0800

    db working

--- routes.js
+++ routes.js
@@ -1,22 +1,20 @@
 let express = require('express');
-let model = require('./Database/db.js');
+let A5_DB = require('./Database/A5_DB.js');
 
 
 let router  = express.Router();
-
+var mongoUrl = "mongodb://monitor:nopassword@cluster0-shard-00-00-h9gia.mongodb.net:27017,cluster0-shard-00-01-h9gia.mongodb.net:27017,cluster0-shard-00-02-h9gia.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+var dbName = "a5db";
 
 router.route('/getData')
      .get((req,res) => {
-         var dbPromise = model.getData();
-         dbPromise.then(
-             function(content){
-                 console.log(content);
-                 res.send(content);
-             },
-             function(err){
-                res.send("error get calendar: " + err);
-            }
-         );
+         //var dbPromise = model.getData();
+        var db = A5_DB(mongoUrl, dbName);       
+        var products = db.getProducts();
+        products.then(function(arg){
+            //console.log(arg);
+            res.send(arg);
+        })
      }
 );
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const exphbs = require('express-handlebars');
-
+let model = require('./Database/A5_DB.js');
 
 var port = process.env.PORT || 3000;
 
@@ -11,5 +11,6 @@ app.use(express.json());
 // Route Files
 let router = require('./routes');
 app.use(router);
+
 
 app.listen(port, () => console.log("SERVER STARTED..."));
