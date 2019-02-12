cea24c37d595bf1b32b4e0a016d9b434610cf6cc
Harshmneet Arora <harshmeet_arora@yahoo.com>
2019-02-11 20:41:29 -0800

    new implementation

--- routes.js
+++ routes.js
@@ -3,22 +3,20 @@ let A5_DB = require('./Database/A5_DB.js');
 
 
 let router  = express.Router();
-var mongoUrl = "mongodb://monitor:nopassword@cluster0-shard-00-00-h9gia.mongodb.net:27017,cluster0-shard-00-01-h9gia.mongodb.net:27017,cluster0-shard-00-02-h9gia.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+var mongoUrl = "mongodb://a5db:nopassword@cluster0-shard-00-00-4d2tn.mongodb.net:27017,cluster0-shard-00-01-4d2tn.mongodb.net:27017,cluster0-shard-00-02-4d2tn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 var dbName = "a5db";
 
 router.route('/getData')
      .get((req,res) => {
-         //var dbPromise = model.getData();
-        var db = A5_DB(mongoUrl, dbName);       
-        var products = db.getProducts();
-        products.then(function(arg){
-            // Website you wish to allow to connect
+        const collection = req.app.locals.collection;
+        var responsePromise = collection.find({}).toArray()
+        responsePromise.then(function(content){
             res.setHeader('Access-Control-Allow-Origin', '*');
             // Request methods you wish to allow
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-            res.send(arg);
-        })
-     }
-);
+            res.send(content);
+        }
+        );
+        });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,10 +7,22 @@ var port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-
 // Route Files
 let router = require('./routes');
 app.use(router);
 
+var MongoClient = require('mongodb').MongoClient;
+var mongo_uri = "mongodb://a5db:nopassword@cluster0-shard-00-00-4d2tn.mongodb.net:27017,cluster0-shard-00-01-4d2tn.mongodb.net:27017,cluster0-shard-00-02-4d2tn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 
-app.listen(port, () => console.log("SERVER STARTED..."));
+MongoClient.connect(mongo_uri, { useNewUrlParser: true })
+.then(client => {
+  const db = client.db('a5db');
+  const collection = db.collection('time_logs');
+  app.locals.collection = collection;
+  app.listen(port, () => console.info(`REST API running on port ${port}`));
+}).catch(error => console.error(error));
+
+
+
+
+// app.listen(port, () => console.log("SERVER STARTED..."));
